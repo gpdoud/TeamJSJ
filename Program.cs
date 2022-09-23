b15503216d268f4bd6f046038f4d31c23c30f6d2
Sam Schultz <sam.schultz@outlook.com>
2022-09-23 10:53:33 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseCors(x =>
+{
+    x.AllowAnyHeader()
+     .AllowAnyOrigin()
+     .AllowAnyMethod();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
