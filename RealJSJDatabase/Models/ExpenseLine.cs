873ee19be1f586e519b3f172461973e985c522cd
Jack Lambert <jackwlambert14@gmail.com>
2022-09-23 13:42:31 -0400

    ExpenseLine Model 1.0

---
+++ RealJSJDatabase/Models/ExpenseLine.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealJSJDatabase.Models {
+
+    public class ExpenseLine {
+
+        public int Id { get; set; }
+
+        [StringLength(80)]
+        public string Description { get; set; }
+
+        [StringLength(10)]
+        public string Status { get; set; } = "NEW";
+
+        [Column(TypeName = "Decimal (11,2)")]
+        public decimal Total { get; set; } = 0;
+
+
+        public int EmployeeId { get; set; }
+        public virtual Employee? Employee { get; set; } //FK
+
+    }
+}
