531e2377f4ee60330c1f2f22a70da39fae555fb8
Sam Schultz <sam.schultz@outlook.com>
2022-09-23 12:02:03 -0400

    AddedDbContextandPackages

---
+++ RealJSJDatabase/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealJSJDatabase.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
