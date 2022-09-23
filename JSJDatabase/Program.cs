0452033bd6781872d02a9238d4efae4c479145ca
Sam Schultz <sam.schultz@outlook.com>
2022-09-23 11:21:04 -0400

    Init

--- JSJDatabase/Program.cs
+++ JSJDatabase/Program.cs
@@ -3,11 +3,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(x =>
+{
+    x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin();
+});
 app.UseAuthorization();
 
 app.MapControllers();
